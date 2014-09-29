5a062a7b49f3fc85d163375b3c7b63852f518140
Anders Lundström <anders@urkraft.se>
2014-09-29 15:04:58 +0200

    Nu går det att uppdatera profilen

--- public/app/account/mvProfileCtrl.js
+++ public/app/account/mvProfileCtrl.js
@@ -1,6 +1,24 @@
-function mvProfileCtrl(mvAuth, mvIdentity, mvNotify) {
+function mvProfileCtrl($scope, mvAuth, mvIdentity, mvNotifier) {
+  $scope.currentuser = mvIdentity.currentUser;
   $scope.email = mvIdentity.currentUser.username;
   $scope.fname = mvIdentity.currentUser.firstName;
-  $scope.fname = mvIdentity.currentUser.lastName;
+  $scope.lname = mvIdentity.currentUser.lastName;
 
+  $scope.update = function () {
+    var newUserData = {
+      username: $scope.email,
+      firstName: $scope.fname,
+      lastName: $scope.lname
+    };
+
+    if ($scope.password && $scope.password.length > 0) {
+      newUserData.password = $scope.password;
+    }
+
+    mvAuth.updateCurrentUser(newUserData).then(function () {
+      mvNotifier.notify('Your user account has been updated');
+    }, function (reason) {
+      mvNotifier.error(reason);
+    })
+  }
 }
