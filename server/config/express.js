bdf16fada1bcfa883c853ab0c7320787d6a70c11
Anders Lundström <anders@urkraft.se>
2014-09-29 17:58:06 +0200

    deprecated-varningar lösta

--- server/config/express.js
+++ server/config/express.js
@@ -17,8 +17,11 @@ module.exports = function(app, config){
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'ejs' );
     app.use(cookieParser());
-    app.use(bodyParser());
-    app.use(session({secret: 'multi vision unicorns'}));
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+    app.use(session({secret: 'multi vision unicorns',
+                     resave: true,
+                     saveUninitialized: true}));
     app.use(passport.initialize());
     app.use(passport.session());
     app.use(stylus.middleware({
