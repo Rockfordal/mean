2bfe4ef5cbb42ca8f97f957f19ae355e3b04aeae
Anders Lundström <anders@urkraft.se>
2014-09-29 17:46:50 +0200

    Express 4.0 uppgraderad!!! (klagar dock på deprecated body-parser, express-session)

--- server/config/express.js
+++ server/config/express.js
@@ -1,29 +1,31 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
     stylus = require('stylus'),
+    passport = require('passport');
     //cors = require('cors'),
-    passport = require('passport'),
-    fs = require('fs');
+    //fs = require('fs');
+
+var app = express();
 
 module.exports = function(app, config){
     function compile(str, path){
         return stylus(str).set('filename', path);
     }
 
-  app.configure(function(){
-        app.set('views', config.rootPath + '/server/views');
-        app.set('view engine', 'ejs' );
-        app.use(express.bodyParser());
-        app.use(express.cookieParser());
-        app.use(express.bodyParser());
-        app.use(express.session({secret: 'multi vision unicorns'}));
-        app.use(passport.initialize());
-        app.use(passport.session());
-        app.use(stylus.middleware({
-                src: config.rootPath + '/public',
-                compile: compile
-            }
-        ));
-        app.use(express.static(config.rootPath + '/public'));
-        //app.use(cors());
-    });
+    app.set('views', config.rootPath + '/server/views');
+    app.set('view engine', 'ejs' );
+    app.use(cookieParser());
+    app.use(bodyParser());
+    app.use(session({secret: 'multi vision unicorns'}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(stylus.middleware({
+            src: config.rootPath + '/public',
+            compile: compile
+        }
+    ));
+    app.use(express.static(config.rootPath + '/public'));
+    //app.use(cors());
 };
