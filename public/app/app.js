2f2d10caeb21e660254489e46370b5292c5cbcc8
Anders Lundström <anders@urkraft.se>
2014-09-29 19:19:44 +0200

    bytt till ui-router (OBS, lyckades tyvärr inte läsa av felmeddelandet från mvAuth)

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngResource', 'ngRoute', 'ui.keypress']);
+var app = angular.module('app', ['ngResource', 'ui.router', 'ui.keypress']);
 
 app.value('mvToastr', toastr);
 app.controller('mvNavBarLoginCtrl', mvNavBarLoginCtrl);
@@ -10,7 +10,7 @@ app.factory('mvAuth', mvAuth);
 app.factory('mvUser', mvUser);
 app.factory('mvUserListCtrl', mvUserListCtrl);
 
-app.config(function ($routeProvider, $locationProvider) {
+app.config(function ($stateProvider, $locationProvider) {
   var routeRoleChecks = {
     admin: {auth:
       function (mvAuth) {
@@ -23,21 +23,21 @@ app.config(function ($routeProvider, $locationProvider) {
   };
 
   $locationProvider.html5Mode(true);
-  $routeProvider
-    .when('/', {templateUrl: '/app/main/main.html', controller: 'mvMainCtrl'})
-    .when('/admin/users', {templateUrl: '/app/admin/user-list.html',
-      controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
-    })
-    .when('/signup', {templateUrl: '/app/account/signup.html',
-      controller: 'mvSignupCtrl' })
-    .when('/profile', {templateUrl: '/app/account/profile.html',
+  $stateProvider
+    .state('home', {url: '/', templateUrl: '/app/main/main.html', controller: 'mvMainCtrl'})
+    .state('useradmin', { url: '/admin/users', templateUrl: '/app/admin/user-list.html',
+                          controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin })
+    .state('signup', { url: '/signup', templateUrl: '/app/account/signup.html',
+                       controller: 'mvSignupCtrl' })
+    .state('profile', { url: '/profile', templateUrl: '/app/account/profile.html',
       controller: 'mvProfileCtrl', resolve: routeRoleChecks.user });
 });
 
-app.run(function ($rootScope, $location) {
-  $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
-    if (rejection === 'not authorized') {
-      $location.path('/');
+app.run(function ($rootScope, $state, mvNotifier) {
+  $rootScope.$on('$stateChangeError', function (event, toState, fromState, error) {
+    if (error) {
+      mvNotifier.error('Sorry, you are probably not allowed here');
+      $state.go('home');
     }
   })
 });
