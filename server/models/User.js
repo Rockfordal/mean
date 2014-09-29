784e89297852a4dccff3b9126067342119912eed
Anders Lundström <anders@urkraft.se>
2014-09-29 11:31:34 +0200

    skapa endast admin som user

--- server/models/User.js
+++ server/models/User.js
@@ -23,20 +23,11 @@ function createDefaultUsers() {
     if (collection.length === 0) {
       var salt, hash;
       salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'andersl');
-      var andersl = {firstName: 'Anders', lastName: 'Lundström', username: 'andersl', salt: salt, hashed_pwd: hash, roles: ['admin']};
-      User.create(andersl, function(err){
+      hash = encrypt.hashPwd(salt, 'admin');
+      var admin = {firstName: 'Admin', lastName: 'User', username: 'admin', salt: salt, hashed_pwd: hash, roles: ['admin']};
+      User.create(admin, function (err) {
         if (err) console.log('fel: ' + err);
       });
-
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'hakan');
-      User.create({firstName: 'Håkan', lastName: 'Gustafsson',  username: 'hakan', salt: salt, hashed_pwd: hash, roles: ['admin']});
-
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'mattias');
-      User.create({firstName: 'Mattias',  lastName: 'Östholm', username: 'mattias', salt: salt, hashed_pwd: hash, roles: []});
-      console.log('har skapat användare');
     }
   })
 }
