e7a9b6d1f6324e2d4eaf63083584f0df95bb2dda
Anders Lundström <anders@urkraft.se>
2014-10-09 00:55:01 +0200

    define för mvSignupCtrl

--- public/app/account/mvSignupCtrl.js
+++ public/app/account/mvSignupCtrl.js
@@ -1,17 +1,22 @@
-function mvSignupCtrl($scope, mvUser, mvNotifier, $location, mvAuth) {
-  $scope.signup = function () {
-    var newUserData = {
-      username: $scope.email,
-      password: $scope.password,
-      firstName: $scope.fname,
-      lastName: $scope.lname
-    };
+define([], function () {
 
-    mvAuth.createUser(newUserData).then(function () {
-      mvNotifier.notify('Användaren skapad');
-      $location.path('/');
-    }, function (reason) {
-      mvNotifier.error(reason);
-    })
+  function mvSignupCtrl($scope, mvUser, mvNotifier, $location, mvAuth) {
+    $scope.signup = function () {
+      var newUserData = {
+        username:  $scope.email,
+        password:  $scope.password,
+        firstName: $scope.fname,
+        lastName:  $scope.lname
+      };
+
+      mvAuth.createUser(newUserData).then(function () {
+        mvNotifier.notify('Användaren skapad');
+        $location.path('/');
+      }, function (reason) {
+        mvNotifier.error(reason);
+      })
+    }
   }
-}
+
+  return mvSignupCtrl;
+});
