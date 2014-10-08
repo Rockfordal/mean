de8497ca1b3f9b9139a2ca0797ab2eeccb4e604e
Anders Lundström <anders@urkraft.se>
2014-10-08 23:44:33 +0200

    toaster bottom full aidth

--- public/app/core/config.js
+++ public/app/core/config.js
@@ -1,5 +1,9 @@
 define(['app'], function (app) {
 
+  mvToastr.options = {
+    positionClass: "toast-bottom-full-width"
+  };
+
   app
     .config(function ($locationProvider) {
       $locationProvider.html5Mode(true);
