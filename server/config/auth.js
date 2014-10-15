f5c0d6ec453c572b31d662fa369e877f398f0071
Anders Lundström <anders@urkraft.se>
2014-10-15 18:47:05 +0200

    skicka user inuti enkodad token

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,20 +1,29 @@
 var passport = require('passport');
+var jwt = require('jsonwebtoken');
 
-exports.authenticate = function(req, res, next) {
+exports.authenticate = function(req, res, next, config) {
   req.body.username = req.body.username.toLowerCase();
   var auth = passport.authenticate('local', function (err, user) {
     if (err) {return next(err);}
-    if (!user) {res.send({success:false})}
+    if (!user) {
+      res.send({success:false});
+    }
     req.logIn(user, function(err) {
-      if (err) {return next(err);}
-      res.send({success:true, user:user});
-    })
+      if (err) {
+        return next(err);
+      }
+
+      // We are sending the profile inside the token
+      var token = jwt.sign(user, config.secret, { expiresInMinutes: 60*5 });
+
+      res.send({success:true, token: token});
+    });
   });
   auth(req, res, next);
 };
 
 exports.requiresApiLogin = function (req, res, next) {
-  if (!req.isAuthenticated())  {
+  if (!req.isAuthenticated()) {
     res.status(403);
     res.end();
   } else {
@@ -30,5 +39,5 @@ exports.requiresRole = function (role) {
     } else {
       next();
     }
-  }
+  };
 };
