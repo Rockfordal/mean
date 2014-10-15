960d9857d98f77adace4faf3390f5baa8cfab8f9
Anders Lundström <anders@urkraft.se>
2014-10-15 18:45:12 +0200

    config secret

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,16 @@ var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
+
   development: {
+    secret: 'tyst123',
     rootPath: rootPath,
     db: 'mongodb://localhost/multivision',
     port: process.env.PORT || 3030
   },
+
   production: {
+    secret: 'tyst123',
     rootPath: rootPath,
     db: 'mongodb://localhost/multivision',
     port: process.env.PORT || 3030
