a61ec6f3e4dbe50d1d7e3637d9a86994debba751
Anders Lundström <anders@urkraft.se>
2014-10-21 15:45:31 +0200

    rensa Todo

--- public/app/todo/mvTodoCtrl.js
+++ public/app/todo/mvTodoCtrl.js
@@ -1,35 +1,13 @@
 define([], function () {
 
-  function mvTodoCtrl(mvTodo, $filter) {
+  function mvTodoCtrl(mvTodo, $filter, mvNotifier) {
     var vm = this;
-    vm.todos = mvTodo.$collection();
-    vm.currentPage = 1;
-    vm.itemsPerPage = 5;
-    vm.maxPagesToShow = 6;
-    getTodos();
 
-    function getTodos() {
-      vm.todos.$fetch();
-      vm.todos.$then(function () {
-        vm.totalItems = vm.todos.length;
-        vm.pageChanged();
-      });
-    }
-
-    vm.setPage = function (pageNo) {
-      vm.currentPage = pageNo;
+    vm.flasha = function() {
+      console.log('flashar');
+      mvNotifier.notify('testar');
     };
 
-    vm.pageChanged = function () {
-      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
-      var end = begin + vm.itemsPerPage;
-      var todos = $filter('filter')(vm.todos, vm.searchText);
-      vm.filttodos = todos.slice(begin, end);
-    };
-
-    vm.pageCount = function () {
-      return Math.ceil(vm.todos.length / vm.itemsPerPage);
-    };
   }
 
   return mvTodoCtrl;
